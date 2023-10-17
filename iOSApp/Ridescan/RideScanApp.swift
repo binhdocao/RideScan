dde9d66db409bfac6360bd2aa66c3547c324619f
Gage Broberg <gagebroberg@tamu.edu>
2023-10-17 10:12:12 -0500

    Adding mongodb basics.

---
+++ iOSApp/Ridescan/RideScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideScanApp.swift
+//  Ridescan
+//
+//  Created by Gage Broberg on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RideScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UserProfile()
+        }
+    }
+}
---
+++ ridescan/ridescan/Views/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  ridescan
+//
+//  Created by Gage Broberg on 10/9/23.
+//
+
+import Foundation
