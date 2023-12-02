3c4d433c7fa3ab4f398f07adae82ab84850f29e7
binhdocao <binhdocao@tamu.edu>
2023-12-02 16:04:10 -0600

    moved index.js

--- ridescan-web/src/Admin.js
+++ ridescan-web/src/Admin.js
@@ -20,13 +20,40 @@ function AdminPage() {
             .catch(error => console.error('Error approving service', error));
     };
 
+    const adminPageStyle = {
+        padding: '20px',
+        textAlign: 'center'
+    };
+
+    const serviceCardStyle = {
+        backgroundColor: 'white',
+        borderRadius: '5px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        padding: '20px',
+        margin: '10px auto',
+        maxWidth: '400px'
+    };
+
+    const buttonStyle = {
+        backgroundColor: '#800000', // Maroon color
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        marginTop: '10px',
+    };
+
     return (
-        <div>
+        <div style={adminPageStyle}>
             <h1>Admin Page</h1>
             {proposedServices.map(service => (
-                <div key={service._id}>
+                <div key={service._id} style={serviceCardStyle}>
                     <h2>{service.serviceName}</h2>
-                    <button onClick={() => handleApprove(service._id)}>Approve</button>
+                    <p>{service.contactName}</p>
+                    <p>{service.email}</p>
+                    {/* Include other service details as needed */}
+                    <button style={buttonStyle} onClick={() => handleApprove(service._id)}>Approve</button>
                 </div>
             ))}
         </div>
